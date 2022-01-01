43e84da42d6eea7199f27ef81e289d1ec57f51d7
陳以新 <elsie96124@gmail.com>
2022-01-01 10:00:14 +0800

    Test0101

--- app.py
+++ app.py
@@ -12,7 +12,6 @@ from utils import send_text_message
 
 load_dotenv()
 
-
 machine = TocMachine(
     states=["user", "state1", "state2"],
     transitions=[
@@ -24,11 +23,15 @@ machine = TocMachine(
         },
         {
             "trigger": "advance",
-            "source": "user",
+            "source": "state1",
             "dest": "state2",
             "conditions": "is_going_to_state2",
         },
-        {"trigger": "go_back", "source": ["state1", "state2"], "dest": "user"},
+        {
+            "trigger": "go_back",
+            "source": ["state1", "state2"],
+            "dest": "user"
+        },
     ],
     initial="user",
     auto_transitions=False,
